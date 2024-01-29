4a88642d06c14533788154d046bfc1bf52e0b6be
Ricardo Navarro <ricardodgcvisual@gmail.com>
2024-01-29 10:04:28 -0600

    adelanto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { CasosComponent } from './casos/casos.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+//En este arrat colocaremos nuestras rutas para generar una SPA (cada ruta sera un objeto)
+const routes: Routes = [
+  {path:'servicios', component:ServiciosComponent},
+  {path:'casos', component:CasosComponent},
+  {path:'nosotros', component:NosotrosComponent },
+  {path:'certificaciones', component:CertificacionesComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'**', redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { CasosComponent } from './casos/casos.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BlogComponent } from './blog/blog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiciosComponent,
+    CasosComponent,
+    NosotrosComponent,
+    CertificacionesComponent,
+    ContactoComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,// esto es para formularios de tipo plantilla
+    ReactiveFormsModule // esto es para formularios de tipos reativos
   ],
   providers: [],
   bootstrap: [AppComponent]
